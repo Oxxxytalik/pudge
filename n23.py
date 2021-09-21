36a34a344258729a3d4700b7dba3be78545fdb00
OxxxyTalik <nesterov.anatolii@phystech.edu>
2021-09-21 22:33:15 +0300

    2+3

---
+++ n23.py
@@ -0,0 +1,243 @@
+import pygame
+from pygame.draw import *
+pygame.init()
+FPS = 30
+screen = pygame.display.set_mode((600,1000))
+white = [255, 255, 255]
+screen.fill(white)
+
+
+rect(screen, (0, 255, 0), (0, 450, 600, 600))
+rect(screen, (0, 255, 255), (0, 0, 600, 450))
+
+rect(screen, (204, 254, 254), (130, 500, 60, 120))
+pygame.draw.ellipse(screen, (0,255,0),(72, 179, 120, 92))
+pygame.draw.ellipse(screen, (0,153,0),(73, 180, 118, 90))
+pygame.draw.ellipse(screen, (0,255,0),(112, 159, 152, 172))
+pygame.draw.ellipse(screen, (0,153,0),(113, 160, 150, 170))
+
+pygame.draw.ellipse(screen, (0,255,0), (-16, 239, 362, 172))
+pygame.draw.ellipse(screen, (0,153,0),(-15, 240, 360, 170))
+
+pygame.draw.ellipse(screen, (0,255,0), (50, 379, 222, 152))
+pygame.draw.ellipse(screen, (0,153,0),(51, 380, 220, 150))
+pygame.draw.ellipse(screen, (0,255,0),  (139, 498, 27, 25))
+pygame.draw.ellipse(screen, (253,155,255),  (140, 499, 25, 23))
+pygame.draw.ellipse(screen, (0,255,0), (159, 248, 27, 25))
+pygame.draw.ellipse(screen, (253,155,255),  (160, 249, 25, 23))
+pygame.draw.ellipse(screen, (0,255,0),(108, 350, 27, 25))
+pygame.draw.ellipse(screen, (253,155,255),  (109, 351, 25, 23))
+pygame.draw.ellipse(screen, (0,255,0), (228, 280, 20, 20))
+pygame.draw.ellipse(screen, (253,155,255),(229, 281, 18, 18))
+rect(screen, (204, 254, 254), (30, 700, 60, 120))
+pygame.draw.ellipse(screen, (0,255,0),(12, 509, 102, 172))
+pygame.draw.ellipse(screen, (0,153,0),(13, 510, 100, 170))
+pygame.draw.ellipse(screen, (0,255,0), (-16, 609, 162, 152))
+pygame.draw.ellipse(screen, (0,153,0),(-15, 610, 160, 150))
+pygame.draw.ellipse(screen, (0,255,0), (0, 679, 122, 102))
+pygame.draw.ellipse(screen, (0,153,0),(1, 680, 120, 100))
+pygame.draw.ellipse(screen, (0,255,0),  (39, 698, 27, 25))
+pygame.draw.ellipse(screen, (253,155,255),  (40, 699, 25, 23))
+pygame.draw.ellipse(screen, (0,255,0), (59, 648, 27, 25))
+pygame.draw.ellipse(screen, (253,155,255),  (60, 649, 25, 23))
+pygame.draw.ellipse(screen, (0,255,0),(8, 650, 27, 25))
+pygame.draw.ellipse(screen, (253,155,255),  (9, 651, 25, 23))
+pygame.draw.ellipse(screen, (0,255,0), (28, 580, 20, 20))
+pygame.draw.ellipse(screen, (253,155,255),(29, 581, 18, 18))
+
+rect(screen, (204, 254, 254), (90, 800, 20, 80))
+pygame.draw.ellipse(screen, (0,255,0),(72, 659, 62, 122))
+pygame.draw.ellipse(screen, (0,153,0),(73, 660, 60, 120))
+pygame.draw.ellipse(screen, (0,255,0), (44, 739, 122, 62))
+pygame.draw.ellipse(screen, (0,153,0),(45, 740, 120, 60))
+pygame.draw.ellipse(screen, (0,255,0), (60, 779, 82, 62))
+pygame.draw.ellipse(screen, (0,153,0),(61, 780, 80, 60))
+pygame.draw.ellipse(screen, (0,255,0),  (99, 798, 37, 25))
+pygame.draw.ellipse(screen, (253,155,255),  (100, 799, 35, 23))
+pygame.draw.ellipse(screen, (0,255,0), (119, 748, 27, 25))
+pygame.draw.ellipse(screen, (253,155,255),  (120, 749, 25, 23))
+pygame.draw.ellipse(screen, (0,255,0),(68, 750, 27, 25))
+pygame.draw.ellipse(screen, (253,155,255),  (69, 751, 25, 23))
+pygame.draw.ellipse(screen, (0,255,0), (88, 680, 20, 20))
+pygame.draw.ellipse(screen, (253,155,255),(89, 681, 18, 18))
+
+rect(screen, (204, 254, 254), (50, 900, 20, 80))
+rect(screen, (255, 255, 255), (280, 860, 15, 80))
+pygame.draw.ellipse(screen, (0,153,0),(33, 760, 60, 120))
+pygame.draw.ellipse(screen, (0,255,0), (4, 839, 122, 62))
+pygame.draw.ellipse(screen, (0,153,0),(5, 840, 120, 60))
+pygame.draw.ellipse(screen, (0,255,0), (20, 879, 82, 62))
+pygame.draw.ellipse(screen, (0,153,0),(21, 880, 80, 60))
+pygame.draw.ellipse(screen, (0,255,0),  (59, 898, 37, 25))
+pygame.draw.ellipse(screen, (253,155,255),  (60, 899, 35, 23))
+pygame.draw.ellipse(screen, (0,255,0), (79, 848, 27, 25))
+pygame.draw.ellipse(screen, (253,155,255),  (80, 849, 25, 23))
+pygame.draw.ellipse(screen, (0,255,0),(28, 850, 27, 25))
+pygame.draw.ellipse(screen, (253,155,255),  (29, 851, 25, 23))
+pygame.draw.ellipse(screen, (0,255,0), (48, 780, 20, 20))
+pygame.draw.ellipse(screen, (253,155,255),(49, 781, 18, 18))
+
+
+
+rect(screen, (255, 255, 255), (280, 860, 15, 80))
+rect(screen, (255, 255, 255), (320, 850, 18, 80))
+rect(screen, (255, 255, 255), (400, 860, 15, 80))
+rect(screen, (255, 255, 255), (420, 845, 15, 80))
+pygame.draw.ellipse(screen, (255, 255, 255), (270, 800, 180, 90))
+rect(screen, (255, 255, 255), (370, 760, 65, 60))
+pygame.draw.ellipse(screen, (255, 255, 255), (380, 720, 70, 50))
+pygame.draw.ellipse(screen, (255, 255, 255), (410, 730, 70, 30))
+pygame.draw.ellipse(screen, (153, 0, 255), (420, 735, 16, 16))
+pygame.draw.ellipse(screen, (255, 255, 255), (420, 735, 8, 4))
+pygame.draw.ellipse(screen, (0, 0, 0), (427, 740, 8, 8))
+polygon(screen, (253, 151, 185), [(420,730), (430,650),
+                               (410,730), (430,730)])
+
+
+pygame.draw.ellipse(screen, (255, 153, 153), (390, 715, 30, 15 ))
+pygame.draw.ellipse(screen, (245, 153, 153), (375, 728, 40, 15 ))
+pygame.draw.ellipse(screen, (215, 153, 153), (385, 740, 35, 15 ))
+pygame.draw.ellipse(screen, (205, 205, 153), (375, 728, 40, 15 ))
+pygame.draw.ellipse(screen, (245, 153, 153), (360, 738, 35, 30 ))
+pygame.draw.ellipse(screen, (255, 253, 153), (345, 764, 65, 25 ))
+pygame.draw.ellipse(screen, (205, 153, 253), (390, 780, 30, 15 ))
+pygame.draw.ellipse(screen, (185, 253, 53), (365, 787, 40, 20 ))
+pygame.draw.ellipse(screen, (80, 153, 203), (325, 787, 50, 25 ))
+
+
+pygame.draw.ellipse(screen, (255, 253, 153), (245, 860, 65, 25 ))
+pygame.draw.ellipse(screen, (245, 153, 153), (225, 828, 50, 25 ))
+pygame.draw.ellipse(screen, (205, 153, 253), (235, 848, 40, 20 ))
+pygame.draw.ellipse(screen, (185, 253, 53), (200, 855, 50, 25 ))
+pygame.draw.ellipse(screen, (225, 153, 153), (200, 845, 40, 15 ))
+pygame.draw.ellipse(screen, (205, 153, 253), (200, 855, 30, 15 ))
+pygame.draw.ellipse(screen, (185, 253, 53), (175, 870, 40, 20 ))
+pygame.draw.ellipse(screen, (80, 153, 203), (155, 885, 50, 25 ))
+
+pygame.draw.ellipse(screen, (255, 253, 153), (215, 870, 65, 25 ))
+pygame.draw.ellipse(screen, (245, 153, 153), (225, 890, 50, 25 ))
+pygame.draw.ellipse(screen, (205, 153, 253), (235, 905, 40, 20 ))
+pygame.draw.ellipse(screen, (185, 253, 53), (200, 910, 50, 25 ))
+
+
+
+
+
+rect(screen, (255, 255, 255), (280, 540, 15, 60))
+rect(screen, (255, 255, 255), (320, 530, 18, 60))
+rect(screen, (255, 255, 255), (400, 540, 15, 60))
+rect(screen, (255, 255, 255), (420, 520, 15, 60))
+pygame.draw.ellipse(screen, (255, 255, 255), (270, 500, 160, 70))
+rect(screen, (255, 255, 255), (370, 460, 65, 60))
+pygame.draw.ellipse(screen, (255, 255, 255), (380, 420, 70, 50))
+pygame.draw.ellipse(screen, (255, 255, 255), (410, 430, 70, 30))
+pygame.draw.ellipse(screen, (153, 0, 255), (420, 435, 16, 16))
+pygame.draw.ellipse(screen, (255, 255, 255), (420, 435, 8, 4))
+pygame.draw.ellipse(screen, (0, 0, 0), (427, 440, 8, 8))
+polygon(screen, (253, 151, 185), [(420,430), (430,250),
+                               (410,430), (430,430)])
+
+
+pygame.draw.ellipse(screen, (255, 153, 153), (390, 415, 30, 15 ))
+pygame.draw.ellipse(screen, (245, 153, 153), (375, 428, 40, 15 ))
+pygame.draw.ellipse(screen, (215, 153, 153), (385, 440, 35, 15 ))
+pygame.draw.ellipse(screen, (205, 205, 153), (375, 428, 40, 15 ))
+pygame.draw.ellipse(screen, (245, 153, 153), (360, 438, 35, 30 ))
+pygame.draw.ellipse(screen, (255, 253, 153), (345, 464, 65, 25 ))
+pygame.draw.ellipse(screen, (205, 153, 253), (390, 480, 30, 15 ))
+pygame.draw.ellipse(screen, (185, 253, 53), (365, 487, 40, 20 ))
+pygame.draw.ellipse(screen, (80, 153, 203), (325, 487, 50, 25 ))
+
+
+pygame.draw.ellipse(screen, (255, 253, 153), (245, 560, 65, 25 ))
+pygame.draw.ellipse(screen, (245, 153, 153), (225, 528, 50, 25 ))
+pygame.draw.ellipse(screen, (205, 153, 253), (235, 548, 40, 20 ))
+pygame.draw.ellipse(screen, (185, 253, 53), (200, 555, 50, 25 ))
+pygame.draw.ellipse(screen, (225, 153, 153), (200, 545, 40, 15 ))
+pygame.draw.ellipse(screen, (205, 153, 253), (200, 555, 30, 15 ))
+pygame.draw.ellipse(screen, (185, 253, 53), (175, 570, 40, 20 ))
+pygame.draw.ellipse(screen, (80, 153, 203), (155, 585, 50, 25 ))
+
+pygame.draw.ellipse(screen, (255, 253, 153), (215, 570, 65, 25 ))
+pygame.draw.ellipse(screen, (245, 153, 153), (225, 590, 50, 25 ))
+pygame.draw.ellipse(screen, (205, 153, 253), (235, 605, 40, 20 ))
+pygame.draw.ellipse(screen, (185, 253, 53), (200, 610, 50, 25 ))
+
+
+
+surface1 = screen.convert_alpha()
+
+surface1.fill([0,0,0,0])
+
+pygame.draw.circle(surface1, (255, 255, 0, 5), (540, 100), 250)
+pygame.draw.circle(surface1, (255, 255, 0, 10), (540, 100), 245)
+pygame.draw.circle(surface1, (255, 255, 0, 15), (540, 100), 240)
+pygame.draw.circle(surface1, (255, 255, 0, 25), (540, 100), 235)
+pygame.draw.circle(surface1, (255, 255, 0, 30), (540, 100), 230)
+pygame.draw.circle(surface1, (255, 255, 0, 35), (540, 100), 225)
+pygame.draw.circle(surface1, (255, 255, 0, 40), (540, 100), 220)
+pygame.draw.circle(surface1, (255, 255, 0, 45), (540, 100), 215)
+pygame.draw.circle(surface1, (255, 255, 0, 50), (540, 100), 210)
+pygame.draw.circle(surface1, (255, 255, 0, 55), (540, 100), 205)
+pygame.draw.circle(surface1, (255, 255, 0, 60), (540, 100), 200)
+pygame.draw.circle(surface1, (255, 255, 0, 65), (540, 100), 195)
+pygame.draw.circle(surface1, (255, 255, 0, 70), (540, 100), 190)
+pygame.draw.circle(surface1, (255, 255, 0, 75), (540, 100), 185)
+pygame.draw.circle(surface1, (255, 255, 0, 80), (540, 100), 180)
+pygame.draw.circle(surface1, (255, 255, 0, 85), (540, 100), 175)
+pygame.draw.circle(surface1, (255, 255, 0, 90), (540, 100), 170)
+pygame.draw.circle(surface1, (255, 255, 0, 95), (540, 100), 165)
+pygame.draw.circle(surface1, (255, 255, 0, 100), (540, 100), 160)
+pygame.draw.circle(surface1, (255, 255, 0, 105), (540, 100), 155)
+pygame.draw.circle(surface1, (255, 255, 0, 110), (540, 100), 150)
+pygame.draw.circle(surface1, (255, 255, 0, 115), (540, 100), 145)
+pygame.draw.circle(surface1, (255, 255, 0, 120), (540, 100), 140)
+pygame.draw.circle(surface1, (255, 255, 0, 125), (540, 100), 135)
+pygame.draw.circle(surface1, (255, 255, 0, 130), (540, 100), 130)
+pygame.draw.circle(surface1, (255, 255, 0, 135), (540, 100), 125)
+pygame.draw.circle(surface1, (255, 255, 0, 140), (540, 100), 120)
+pygame.draw.circle(surface1, (255, 255, 0, 145), (540, 100), 115)
+pygame.draw.circle(surface1, (255, 255, 0, 150), (540, 100), 110)
+pygame.draw.circle(surface1, (255, 255, 0, 155), (540, 100), 105)
+pygame.draw.circle(surface1, (255, 255, 0, 160), (540, 100), 100)
+pygame.draw.circle(surface1, (255, 255, 0, 165), (540, 100), 95)
+pygame.draw.circle(surface1, (255, 255, 0, 170), (540, 100), 90)
+pygame.draw.circle(surface1, (255, 255, 0, 175), (540, 100), 85)
+pygame.draw.circle(surface1, (255, 255, 0, 180), (540, 100), 80)
+pygame.draw.circle(surface1, (255, 255, 0, 185), (540, 100), 75)
+pygame.draw.circle(surface1, (255, 255, 0, 190), (540, 100), 70)
+pygame.draw.circle(surface1, (255, 255, 0, 195), (540, 100), 65)
+pygame.draw.circle(surface1, (255, 255, 0, 200), (540, 100), 60)
+pygame.draw.circle(surface1, (255, 255, 0, 205), (540, 100), 55)
+pygame.draw.circle(surface1, (255, 255, 0, 210), (540, 100), 50)
+pygame.draw.circle(surface1, (255, 255, 0, 215), (540, 100), 45)
+pygame.draw.circle(surface1, (255, 255, 0, 220), (540, 100), 40)
+pygame.draw.circle(surface1, (255, 255, 0, 225), (540, 100), 35)
+pygame.draw.circle(surface1, (255, 255, 0, 230), (540, 100), 30)
+pygame.draw.circle(surface1, (255, 255, 0, 235), (540, 100), 25)
+pygame.draw.circle(surface1, (255, 255, 0, 240), (540, 100), 20)
+pygame.draw.circle(surface1, (255, 255, 0, 245), (540, 100), 15)
+pygame.draw.circle(surface1, (255, 255, 0, 250), (540, 100), 10)
+pygame.draw.circle(surface1, (255, 255, 0, 255), (540, 100), 5)
+
+
+
+
+screen.blit(surface1, (0,0))
+
+
+
+
+
+
+pygame.display.update()
+clock = pygame.time.Clock()
+finished = False
+
+while not finished:
+    clock.tick(FPS)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            finished = True
+
+pygame.quit()
